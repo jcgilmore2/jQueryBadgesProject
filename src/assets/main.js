132254822c4f4d6957c9047d3beb65e54a26b04f
James Gilmore <James.Gilmore@greenwayhealth.com>
2017-01-16 14:23:38 -0500

    added a

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,20 +8,20 @@ $(function() {
         console.log(response);
 
         for ( var i = 0; i < response.courses.completed.length ;  i++ ) {
-
           var $this = response.courses.completed[i];
           var item = {}
           item.title = $this.title;
           item.url = $this.url;
           item.badge = $this.badge;
-
+          //badge.image = $this.img
           var badge =
-          "<a href=' " + item.url + " '  target='_blank' class='course'>" +
+          "<div class='course'>" +
+          "<a href=' " + item.url + " '  target='_blank'>" +
             "<h3>" +  item.title + "</h3>" +
             "<img src='"    + item.badge  +  " '></img " +
-          "</a>";
+            "</a>" +
+          "</div>";
 
-          // Render in DOM
           $("#badges").append(badge)
 
         }
