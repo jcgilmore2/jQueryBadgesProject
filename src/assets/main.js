8626a178e0fc009c1d1e0f82db233ac37d613fc2
James Gilmore <James.Gilmore@greenwayhealth.com>
2017-01-16 14:18:09 -0500

    no message

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,31 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+      url: 'https://www.codeschool.com/users/2133183.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+        console.log(response);
 
+        for ( var i = 0; i < response.courses.completed.length ;  i++ ) {
+
+          var $this = response.courses.completed[i];
+          var item = {}
+          item.title = $this.title;
+          item.url = $this.url;
+          item.badge = $this.badge;
+
+          var badge =
+          "<a href=' " + item.url + " '  target='_blank' class='course'>" +
+            "<h3>" +  item.title + "</h3>" +
+            "<img src='"    + item.badge  +  " '></img " +
+          "</a>";
+
+          // Render in DOM
+          $("#badges").append(badge)
+
+        }
+
+      }
+    });
 });
